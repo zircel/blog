4fd8f6b223f02f0283318d9fcb5aa52fa0ed0700
Roy Rutishauser <roy.rutishauser@gmail.com>
2019-11-17 14:05:23 +0100

    small changes, introducing meta tags

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './style.css'
-import a from './nodes/a'
-import h2 from './nodes/h2'
 
 const body = document.querySelector('#blog-intro')
 const page = document.createElement('zircel-page')
@@ -9,41 +7,10 @@ const page = document.createElement('zircel-page')
 page.pid = '5891d72c07d37b8c12266e8a'
 page.path = '/blog-intro'
 page.scrolling = 'document'
-page.mode = 'production'
+page.mode = process.env.NODE_ENV || 'development'
 
 page.init().catch(err => console.error(err))
 body.appendChild(page)
-
-// highlighing the chapter in the main navigation
-// const chapterName = window.location.pathname.split('/')[2]
-// const navigationItems = Array.from(document.querySelectorAll('#blog-menu a'))
-// navigationItems.forEach(n => {
-//    if (n.href.includes(chapterName)) {
-//       n.classList.add('active')
-//    }
-// })
-
-// // Building the content navigation
-// const navigation = document.querySelector('#blog .chapter-navigation')
-// const titles = Array.from(
-//    document.querySelectorAll('#blog .content h2, #blog .content h3')
-// )
-
-// if (titles.length > 0) {
-//    navigation.appendChild(h2('Contents'))
-// }
-
-// titles.forEach(t => {
-//    // replace all whitespaces with hypens for the url / html id
-//    const id = t.textContent
-//       .toLowerCase()
-//       .split(' ')
-//       .join('-')
-//    navigation.appendChild(
-//       a(`#${id}`, t.textContent, `${t.nodeName.toLowerCase()}-chapter`)
-//    )
-//    t.id = id
-// })
 
 // handling mobile navigation
 const menuburger = document.querySelector('#blog-header .menu-burger')
