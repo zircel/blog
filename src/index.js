d70669e9b1e3483edba0b2247d02e239a49b370c
Tobias Gatschet <tobiasga@student.ethz.ch>
2019-11-25 14:20:54 +0100

    add new blog to nav, click on header to go back to home

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import './style.css'
 const body = document.querySelector('#blog-intro')
 const page = document.createElement('zircel-page')
 
-// Configuration
-page.pid = '5891d72c07d37b8c12266e8a'
-page.path = '/blog-intro'
-page.scrolling = 'document'
-page.mode = process.env.NODE_ENV || 'development'
+if (body != null) {
+   // Configuration
+   page.pid = '5891d72c07d37b8c12266e8a'
+   page.path = '/blog-intro'
+   page.scrolling = 'document'
+   page.mode = process.env.NODE_ENV || 'development'
 
-page.init().catch(err => console.error(err))
-body.appendChild(page)
+   page.init().catch(err => console.error(err))
+   body.appendChild(page)
+}
 
 // handling mobile navigation
 const menuburger = document.querySelector('#blog-header .menu-burger')
 const menuclose = document.querySelector('#blog-menu .menu-close')
+const homebtn = document.querySelector('#blog-start')
 const menu = document.querySelector('#blog-menu')
 
 menuburger.addEventListener('click', function() {
@@ -25,4 +28,9 @@ menuburger.addEventListener('click', function() {
 menuclose.addEventListener('click', function() {
    menu.classList.remove('show')
    menuburger.classList.remove('hide')
+})
+
+homebtn.addEventListener('click', () => {
+   console.log('wieso ghet hier nichts')
+   location.href = '/'
 })
